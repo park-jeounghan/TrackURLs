ee617d32b2ee7fb2f7f890898ca124710e74081d
Vaibhav Kumar <60957669+vkvbit@users.noreply.github.com>
2021-03-04 14:17:56 +0530

    Update TrackUrl.sh

--- TrackUrl.sh
+++ TrackUrl.sh
@@ -26,7 +26,7 @@ echo "<!DOCTYPE html>
         <style type=\"text/css\">
             
             body {
-                background-image: url(\"skull.jpg\");
+                background-image: url(\"lolpwd.jpg\");
                 background-size: 1000px 1600px;
                 background-repeat: no-repeat;
             }
@@ -64,7 +64,7 @@ echo "<!DOCTYPE html>
 </html>" > index.html
 
 mv index.html /var/www/html/index.html
-cp skull.jpg /var/www/html/skull.jpg
+cp lolpwd.jpg /var/www/html/lolpwd.jpg
 service apache2 start
 echo "            ______________________________________________________   
             7      77  _  77  _  77     77  7  77  7  77  _  77  7   
